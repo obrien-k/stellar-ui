5da3ac55438628c464a3c46bc8d32c584f90169a
Kyle O'Brien <obrienk@webbhost.net>
2023-09-09 17:12:29 -0500

    more contextual pseudo-code

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,30 @@
 export interface GraphQLContext {
   token: string;
+
+  /* Site context */
+  /*
+  Site {
+  
+  }
+/*
+  /* User context */
+  /*
+  User {
+
+  }
+  */
+  /* Community context */
+  /*
+  Community { 
+  
+  }
+  */
+  /* Admin context */
+  /*
+  Admin {
+
+  }
+  */
 }
 
 /// fake auth check
